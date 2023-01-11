aa74fd8d58262699e62bd8cef81c26b061b09aca
Jose Eloy <joseeloymontenegro@gmail.com>
2023-01-11 17:49:24 -0600

    Clase 22

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menuHamIcon = document.querySelector('.menu');
 const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shopingCartContainer = document.querySelector('#shopingCartContainer');
 const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
@@ -12,19 +12,19 @@ menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
 // DESKTOP VISTA
 function toggleDesktopMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shopingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed){
-        aside.classList.add('inactive');
+        shopingCartContainer.classList.add('inactive');
     }
     desktopMenu.classList.toggle('inactive');
 }
 // MOBILE VISTA
 function toggleMobileMenu() {
-    const isAsideClosed = aside.classList.contains('inactive');
+    const isAsideClosed = shopingCartContainer.classList.contains('inactive');
 
     if (!isAsideClosed){
-        aside.classList.add('inactive');
+        shopingCartContainer.classList.add('inactive');
     }
     mobileMenu.classList.toggle('inactive');
 }
@@ -35,7 +35,7 @@ function toggleCarritoAside(){
     if (!isMobileMenuClosed){
         mobileMenu.classList.add('inactive');
     }
-    aside.classList.toggle('inactive');
+    shopingCartContainer.classList.toggle('inactive');
 }
 // LIsta de productos
 
@@ -60,28 +60,28 @@ productList.push({
 
 
 for(product of productList){
-    const productCard=document.createElement('div');
+    const productCard = document.createElement('div');
     productCard.classList.add('product-card');
 
-const productImg=document.createElement('img');
+const productImg = document.createElement('img');
 productImg.setAttribute('src',product.image);
 
-const productInfo=document.createElement('div');
+const productInfo = document.createElement('div');
 productInfo.classList.add('product-info');
 
-const productInfoDiv=document.createElement('div');
+const productInfoDiv = document.createElement('div');
 
-const productPrice=document.createElement('p');
-productPrice.innerText='$'+product.price;
+const productPrice = document.createElement('p');
+productPrice.innerText='$' + product.price;
 
-const productName=document.createElement('p');
+const productName = document.createElement('p');
 productName.innerText=product.name;
 
 productInfoDiv.appendChild(productPrice);
 productInfoDiv.appendChild(productName);
 
-const productInfoFigure=document.createElement('figure');
-const productImgCart=document.createElement('img');
+const productInfoFigure = document.createElement('figure');
+const productImgCart = document.createElement('img');
 productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
 
 productInfoFigure.appendChild(productImgCart);
